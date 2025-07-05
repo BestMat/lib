2f136e39f9795cc13d5db0b07d0f119bd86d1a7d
BestMat <tayoyuva@gmail.com>
2025-07-05 19:12:36 +0530

    added colors to window manager

--- terminal.c
+++ terminal.c
@@ -2,6 +2,8 @@
 
 #include <ncurses.h>
 
+#define false 0
+
 // Creates new window
 WINDOW *create_newwin(
     int height,
@@ -11,7 +13,9 @@ WINDOW *create_newwin(
 {
   WINDOW *window;
   window = newwin(height, width, start_y, start_x);
+  wattron(window, COLOR_PAIR(2));
   box(window, 0, 0);
+  wattron(window, COLOR_PAIR(2));
   wrefresh(window);
 
   return window;
@@ -21,16 +25,28 @@ int main() {
   initscr();
   refresh();
 
+  noecho();
+  curs_set(false);
+
   int rows = 0;
   int cols = 0;
 
   getmaxyx(stdscr, rows, cols);
 
+  if (has_colors() == false) {
+    endwin();
+  }
+
+  start_color();
+
+  init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+  init_pair(2, COLOR_CYAN, COLOR_BLACK);
+
   // Creating top window:
   int width = cols;
   int height = rows / 2;
-  int start_x = 0; 
-  int start_y = 0; 
+  int start_x = 0;
+  int start_y = 0;
 
   WINDOW *top = create_newwin(height, width, start_y, start_x);
   
@@ -42,12 +58,17 @@ int main() {
   WINDOW *bot = create_newwin(height, width, start_y, start_x);
 
   // Display text on windows:
+  wattron(top, COLOR_PAIR(1));
   wprintw(top, "Window TOP");
   mvwprintw(top, 1, 2, "Hello from TOP!");
   wrefresh(top);
+  wattroff(top, COLOR_PAIR(1));
+  
+  wattron(bot, COLOR_PAIR(1));
   wprintw(bot, "Window BOT");
   mvwprintw(bot, 1, 2, "Hello from BOT!");
   wrefresh(bot);
+  wattroff(bot, COLOR_PAIR(1));
 
   while (true) {
     char key = getch();
